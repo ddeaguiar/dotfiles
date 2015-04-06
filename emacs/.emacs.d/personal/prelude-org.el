dd69fd097c995643031c592d494780133a5e4145
Daniel De Aguiar <ddeaguiar@pointslope.com>
2015-04-05 21:21:36 -0400

    prelude-org.el ported to use-packages.

--- emacs/.emacs.d/personal/prelude-org.el
+++ emacs/.emacs.d/personal/prelude-org.el
@@ -1,57 +1,44 @@
-;; This should be obvious...
-;; (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(use-package org
+  :init
+  ;; Export
+  (setq org-html-validation-link nil
+        org-export-html-postamble nil
+        org-export-backends '(ascii html icalendar latex md))
+  ;; File handling
+  (setq org-directory "~/Dropbox/org"
+        org-default-notes-file (concat org-directory "/journal.org")
+        org-log-done 'time)
+  ;; TODO
+  (setq org-todo-keywords
+        '((type "NEW" "WIP" "BLOCKED" "PAUSED" "TESTING" "|" "DONE")))
+  :config
+  '(require 'ox-md nil t)
+  (add-hook 'org-mode-hook
+            (lambda ()
+              (flyspell-mode)
+              (writegood-mode)
+              (auto-fill-mode))))
 
-;; Export
-(setq org-html-validation-link nil)
-(setq org-export-html-postamble nil)
+(use-package ob
+  :config
+  (require 'ob)
+  (require 'ob-clojure)
+  (require 'cider)
+  (setq org-babel-clojure-backend 'cider
+        org-fontify-done-headline t
+        org-src-fontify-natively t
+        org-confirm-babel-evaluate nil
+        org-src-window-setup 'current-window)
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((emacs-lisp . t)
+     (sh         . t)
+     (clojure    . t)
+     (java       . t)
+     (js         . t)
+     (ruby       . t)
+     (python     . t)))
+  (add-to-list 'org-babel-tangle-lang-exts '("clojure" . "clj"))
+  (add-to-list 'org-babel-tangle-lang-exts '("js"      . "js")))
 
-;; File handling
-(setq org-directory "~/Dropbox/org")
-(setq org-default-notes-file (concat org-directory "/journal.org"))
-(setq org-log-done 'time)
-
-;; TODO
-(setq org-todo-keywords
-      '((type "NEW" "WIP" "BLOCKED" "PAUSED" "TESTING" "|" "DONE")))
-
-;; Run these, too
-(add-hook 'org-mode-hook
-          (lambda ()
-            (flyspell-mode)))
-
-(add-hook 'org-mode-hook
-          (lambda ()
-            (writegood-mode)))
-
-(add-hook 'org-mode-hook
-          (lambda ()
-            (auto-fill-mode)))
-
-;; Babel
-(require 'ob)
-(require 'ob-clojure)
-(setq org-babel-clojure-backend 'cider)
-(require 'cider)
-(setq org-fontify-done-headline t)
-(setq org-src-fontify-natively t)
-(setq org-confirm-babel-evaluate nil)
-(setq org-src-window-setup 'current-window)
-
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((emacs-lisp . t)
-   (sh         . t)
-   (clojure    . t)
-   (java       . t)
-   (js         . t)
-   (ruby       . t)
-   (python     . t)
-   ;(R          . t)
-   ))
-
-(add-to-list 'org-babel-tangle-lang-exts '("clojure" . "clj"))
-(add-to-list 'org-babel-tangle-lang-exts '("js"      . "js"))
-
-(eval-after-load "org" '(require 'ox-md nil t))
-
-(provide 'prelude-org)
+(provide 'personal/prelude-org)
