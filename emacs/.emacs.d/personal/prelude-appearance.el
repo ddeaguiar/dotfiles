0b3a8813c65896b9d1112d23dc9c0d91d8d6ab7c
Daniel De Aguiar <ddeaguiar@pointslope.com>
2015-04-06 21:38:20 -0400

    Added powerline font-issue documentation to prelude-appearance.el

--- emacs/.emacs.d/personal/prelude-appearance.el
+++ emacs/.emacs.d/personal/prelude-appearance.el
@@ -17,6 +17,7 @@
 
 (use-package cyberpunk-theme
   :init
+  (add-to-list 'default-frame-alist '(font . "Fantasque Sans Mono-18"))
   (disable-theme 'zenburn)
   (menu-bar-mode -1)
   (line-number-mode t)
@@ -29,8 +30,14 @@
 
 ;;; Mode Line
 (use-package powerline
+  :init
+  ;; If you have font-rendering issues with powerline
+  ;; when running emacs in a terminal, you may need to change
+  ;; your Non-ASCII font. See http://input.fontbureau.com/workarounds/
+  (setq powerline-arrow-shape 'arrow)
   :config
   (powerline-center-theme)
+
   (custom-set-faces
    '(powerline-active1 ((t (:foreground "#e0e0e0" :background "#202320" ))))
    '(powerline-active2 ((t (:foreground "#b9d977" :background "#353a3d" ))))))
