9eb915ab58325f7c698b9f9aacd18a607adf7c30
Daniel De Aguiar <ddeaguiar@pointslope.com>
2017-12-31 11:39:19 -0500

    Updated settings

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,3 +1,4 @@
 {:repl
- {:plugins [[cider/cider-nrepl "0.15.1"]
+ {:dependencies [[org.clojure/tools.nrepl "0.2.13"]]
+  :plugins [[cider/cider-nrepl "0.16.0"]
             [refactor-nrepl "2.4.0-SNAPSHOT"]]}}
