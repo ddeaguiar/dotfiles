d11c738f787514e34d6f1a250b195a1a9f46965c
Daniel De Aguiar <ddeaguiar@pointslope.com>
2018-01-07 12:50:05 -0500

    update custom.el based on emacs/prelude update.

--- emacs/.emacs.d/personal/custom.el
+++ emacs/.emacs.d/personal/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(cider-inject-dependencies-at-jack-in nil)
  '(custom-safe-themes
    (quote
     ("71ecffba18621354a1be303687f33b84788e13f40141580fa81e7840752d31bf" "c86f868347919095aa44d2a6129dd714cbcf8feaa88ba954f636295b14ceff8f" "8fed5e4b89cf69107d524c4b91b4a4c35bcf1b3563d5f306608f0c48f580fdf8" "a2e7b508533d46b701ad3b055e7c708323fb110b6676a8be458a758dd8f24e27" "90edd91338ebfdfcd52ecd4025f1c7f731aced4c9c49ed28cfbebb3a3654840b" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "f0a99f53cbf7b004ba0c1760aa14fd70f2eabafe4e62a2b3cf5cabae8203113b" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
@@ -15,7 +16,6 @@
    (quote
     (clj-refactor avy bind-key editorconfig gh git-commit ht magit-popup org paredit s sbt-mode groovy-mode ox-reveal csv-mode git-link key-chord keychord ag with-editor yasnippet aggressive-indent floobits helm-core yaml-mode dockerfile-mode clojure-mode company helm hydra markdown-mode less-css-mode htmlize emmet-mode company-restclient restclient projectile-direnv ace-isearch cljsbuild-mode datomic-snippets clojure-cheatsheet javadoc-lookup clojure-snippets zop-to-char zenburn-theme which-key web-mode volatile-highlights vkill use-package undo-tree unbound smex smartrep smartparens smart-mode-line slime scss-mode rainbow-mode rainbow-identifiers rainbow-delimiters powerline paradox ov operate-on-number multi-term move-text magit json-mode js2-mode inf-ruby indicators imenu-anywhere ido-ubiquitous helm-projectile helm-descbinds helm-ag guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine git-gutter-fringe+ gist ggtags geiser flycheck flx-ido flatland-theme expand-region exec-path-from-shell ensime elisp-slime-nav easy-kill discover-my-major dired+ diff-hl cyberpunk-theme crux company-anaconda cider browse-kill-ring beacon anzu ack ace-window)))
  '(paradox-github-token t)
- '(cider-inject-dependencies-at-jack-in nil)
  '(safe-local-variable-values
    (quote
     ((cider-cljs-lein-repl . "(do (use 'figwheel-sidecar.repl-api) (start-figwheel!) (cljs-repl))")
