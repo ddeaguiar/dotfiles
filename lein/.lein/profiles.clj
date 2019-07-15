fb2591bfa3f1174fa167bfeb1fdc29978d0799dd
Daniel De Aguiar <ddeaguiar@gmail.com>
2019-07-15 13:50:20 -0400

    lein profile tweaks.

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -2,7 +2,7 @@
  :repl     {:src          [sidebar]
             :dependencies [[org.clojure/tools.namespace "0.2.11"]]}
  :socket   {:jvm-opts ["-Dclojure.server.repl={:port,50505,:accept,clojure.core.server/repl}"]}
- :io-prepl {:jvm-opts ["-Dclojure.server.jvm={:port,55555,:accept,clojure.core.server/io-prepl}"]}
+ :io-prepl {:jvm-opts ["-Dclojure.server.jvm={:port,40404,:accept,clojure.core.server/io-prepl}"]}
  :rebl     {:source-paths   ["/Users/ddeaguiar/src/rebl/src"]
             :resource-paths ["/Users/ddeaguiar/src/rebl/resources"]
             :dependencies   [[org.clojure/core.async "0.4.490"]
@@ -10,4 +10,5 @@
             :main           cognitect.rebl}
  :rebl-jar {:dependencies [[com.cognitect/REBL "0.9.172"]
                            [org.clojure/core.async "0.4.490"]]
-            :main         cognitect.rebl}}
+            :main         cognitect.rebl}
+ :socket-rebl {:jvm-opts ["-Dclojure.server.repl={:port,50505,:accept,cognitect.rebl/-main}"]}}
