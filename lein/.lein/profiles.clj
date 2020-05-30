eda56fd9b4f4ebf2981df0d5f4e75d50a36bb9a8
Daniel De Aguiar <ddeaguiar@gmail.com>
2020-05-30 08:28:48 -0400

    Clojure global config updates

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -7,8 +7,11 @@
                                  [org.clojure/spec.alpha "0.2.176"]]
                 :main           cognitect.rebl}
  :rebl-jar     {:dependencies [[com.cognitect/REBL "0.9.172"]
-                               [org.clojure/core.async "0.4.490"]]
+                               [org.clojure/core.async "0.4.490"]
+                               ;; due to project conflicts
+                               [org.clojure/core.memoize "0.8.2"]
+                               [org.clojure/core.cache "0.8.2"]]
                 :main         cognitect.rebl}
- :socket       {:jvm-opts ["-Dclojure.server.repl={:port,50505,:accept,clojure.core.server/repl}"]}
+ :socket       {:jvm-opts ["-Dclojure.server.repl={:port,60606,:accept,clojure.core.server/repl}"]}
  :socket-prepl {:jvm-opts ["-Dclojure.server.io-prepl={:port,40404,:accept,clojure.core.server/io-prepl}"]}
  :socket-rebl  {:jvm-opts ["-Dclojure.server.rebl={:port 60606 :accept cognitect.rebl/-main}"]}}
