0dc2ed63a1bdb0bede746371139ddb96bdd28a1e
Daniel De Aguiar <ddeaguiar@gmail.com>
2021-04-16 17:38:37 -0400

    rbenv fish config

--- shell/.config/fish/config.fish
+++ shell/.config/fish/config.fish
@@ -2,6 +2,7 @@ set -u fish_greeting
 set EDITOR emacs
 
 set PATH $HOME/datomic-cli $HOME/bin $HOME/datomic-pro/bin $HOME/.jenv/bin $HOME/.emacs.d/bin $PATH
+set -x RUBY_CONFIGURE_OPTS "--with-openssl-dir="(brew --prefix openssl@1.1)
 
 status --is-interactive; and source (jenv  init -|psub)
 
@@ -14,5 +15,7 @@ alias lrb="lein with-profiles +rebl,+socket-rebl,+socket-prepl repl"
 alias lsb="lein with-profiles +socket,+socket-prepl repl"
 
 eval (direnv hook fish)
+
+status --is-interactive; and source (rbenv init -|psub)
 
 starship init fish | source
